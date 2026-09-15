1ebfcf787182c5650b5cba699f263b6a72fe65b4
Tasvir Limbani <120560952+TasvirLimbani@users.noreply.github.com>
2026-09-15 12:05:08 +0530

    ads margin and center
    ads margin and center

--- app/ads/AdsterraNative.tsx
+++ app/ads/AdsterraNative.tsx
@@ -13,13 +13,15 @@ export default function GoogleAdsense() {
   }, []);
 
   return (
-    <ins
-      className="adsbygoogle"
-      style={{ display: "block" }}
-      data-ad-client="ca-pub-3990057144186847"
-      data-ad-slot="8487318465"
-      data-ad-format="auto"
-      data-full-width-responsive="true"
-    />
+    <div className="flex justify-center w-full mb-2">
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-3990057144186847"
+        data-ad-slot="8487318465"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      />
+    </div>
   );
 }
--- app/ads/Multiplex.tsx
+++ app/ads/Multiplex.tsx
@@ -3,22 +3,24 @@
 import { useEffect } from "react";
 
 export default function GoogleAutorelaxedAd() {
-  useEffect(() => {
-    try {
-      // @ts-ignore
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
-    } catch (err) {
-      console.error("AdSense error:", err);
-    }
-  }, []);
+    useEffect(() => {
+        try {
+            // @ts-ignore
+            (window.adsbygoogle = window.adsbygoogle || []).push({});
+        } catch (err) {
+            console.error("AdSense error:", err);
+        }
+    }, []);
 
-  return (
-    <ins
-      className="adsbygoogle"
-      style={{ display: "block" }}
-      data-ad-client="ca-pub-3990057144186847"
-      data-ad-slot="8076254925"
-      data-ad-format="autorelaxed"
-    />
-  );
+    return (
+        <div className="flex justify-center w-full my-4">
+            <ins
+                className="adsbygoogle"
+                style={{ display: "block", textAlign: "center" }}
+                data-ad-client="ca-pub-3990057144186847"
+                data-ad-slot="8076254925"
+                data-ad-format="autorelaxed"
+            />
+        </div>
+    );
 }
